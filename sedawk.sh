5fd5eb920501d7e71916e0feb7c6747d0fd92e55
TrackDR <jeremydjackson@gmail.com>
2015-09-23 18:19:24 -0400

    Update sedawk.sh

--- sedawk.sh
+++ sedawk.sh
@@ -5,3 +5,6 @@ sed -i 's/old/new/g' *.txt
 
 # Replace all words and dump to new file
 sed 's/old/new/g' input.txt > output.txt
+
+# Replace paths
+sed 's#/path2replace/bin/#/newpath/sbin/#g' input.txt > output.txt
