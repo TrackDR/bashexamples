01be58838652b46f44aee392a6cc7eec2cd43f9a
TrackDR <jeremydjackson@gmail.com>
2015-10-23 13:53:56 -0400

    Update oneliners.sh

--- oneliners.sh
+++ oneliners.sh
@@ -12,3 +12,9 @@ if [ -d /cygdrive/g/VideoLectures/ShapeSpaces/ ]; then echo "hi"; fi
 # iterate over range
 END=10;for i in $(seq 1 $END); do echo $i; done
 INCR=2;END=10;for i in $(seq 1 $INCR $END); do echo $i; done
+
+#identify images (pngs) in a dir
+for item in `ls *.png`; do identify $item; done
+
+#crop all images (pngs) in a dir
+for item in `ls *.png`; do convert $item -crop 256x256+0+0 ../new_test_images/$item; echo $item; done
