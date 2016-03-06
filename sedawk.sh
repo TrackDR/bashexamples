7ea024e43715a98952873bfbfe0b796e402bb6cb
TrackDR <jeremydjackson@gmail.com>
2016-03-06 18:04:52 -0500

    Update sedawk.sh

--- sedawk.sh
+++ sedawk.sh
@@ -8,3 +8,6 @@ sed 's/old/new/g' input.txt > output.txt
 
 # Replace paths
 sed 's#/path2replace/bin/#/newpath/sbin/#g' input.txt > output.txt
+
+# Extract first column from csv file and change line feeds to spaces
+cat input.csv | awk '{print $1}' | sed ':a;N;$!ba;s/\n/ /g' > output.txt
