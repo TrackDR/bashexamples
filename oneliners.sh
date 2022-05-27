e797418def2c4b4f03cdfed5ca9a57036190a964
TrackDR <jeremydjackson@gmail.com>
2022-05-27 12:38:17 -0400

    Update oneliners.sh

--- oneliners.sh
+++ oneliners.sh
@@ -54,3 +54,6 @@ rename 's/-new//g' *.las
 FLDR1="F:\\myfldr1; FLDR2="F:\\myfldr2; FLDR3="F:\\myfldr3; for x in `ls $FLDR1`; do a=$FLDR1\\$x; b=$FLDR2\\$x; c=$FLDR3\\$x; echo $a; echo $b; echo $c; ls $a; ls $b; convert $a $b +append $c; done
    
 for item in `ls *.bin`; do filename=$(basename "$item"); extension="${filename##*.}"; filename="${filename%.*}"; echo "converting $filename"; mv $filename.$extension $filename.raw ; done
+
+# Extract latest commit number from git log to n places
+n=7;commitnum=$(git log | head -1 | cut -f2 -d' ');commitnum=${commitnum:0:$n}; echo $commitnum
