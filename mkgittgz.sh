9c77eead9450413fda5e9c2e0d38a252772a596f
TrackDR <jeremydjackson@gmail.com>
2022-05-31 02:08:25 -0400

    Create mkgittgz.sh

---
+++ mkgittgz.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+projname="desmume"
+
+parentdir=/cygdrive/f/$projname-git
+gitdir=$projname
+
+pushd $parentdir/$gitdir
+
+commitdate=$(git log -1 --format="%at" | xargs -I{} date -d @{} +%Y-%m-%d)
+echo $commitdate
+
+n=7
+commitnum=$(git log | head -1 | cut -f2 -d' ');commitnum=${commitnum:0:$n}
+echo $commitnum
+
+newgitdir=$gitdir-$commitdate-$commitnum
+
+pushd $parentdir
+cp -R $gitdir $newgitdir
+tar zcf $newgitdir.tgz $newgitdir
+
+popd
+popd
